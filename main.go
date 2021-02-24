048e298fc73b3acb03ed99777866c5205ae6b5ef
OneBoSS2018 <bob204981@gmail.com>
2021-02-24 17:50:43 +0200

    1,2,3,4 ready

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
+)
 
-func main()  {
-	fmt.Println("Hello, NIX Education")
+func main() {
+	var wg sync.WaitGroup
+	for i:=1;i<=100;i++{
+		wg.Add(1)
+		go func(i int) {
+			getPost(i)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
 }
+
+func getPost(s int)  {
+	url:="https://jsonplaceholder.typicode.com/posts/"+ strconv.Itoa(s)
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+
+	}
+	fmt.Printf("%T", err)
+	//We Read the response body on the line below.
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//Convert the body to type string
+	result := string(body)
+	fmt.Println(result)
+}
+
+
